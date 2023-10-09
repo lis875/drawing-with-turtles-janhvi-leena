98021f3634fdf7a70ae2d417f2727a1c38a17527
Leena <leena@wisc.edu>
2023-10-09 13:41:15 -0500

    Added the star function

--- stars.py
+++ stars.py
@@ -2,9 +2,20 @@ from turtle import *
 
 ### Functions
 # Modify code in this section
+speed(0)
 
 def star():
-    pass
+    for _ in range(5):
+    # Loop to draw a single star
+        for _ in range(1):
+            forward(30)  # Move forward by 100 units
+            right(144)    # Turn right by 144 degrees
+    
+    # Move the turtle to the starting position for the next star
+    penup()
+    forward(70)  # Move forward to create space between stars
+    pendown()
+
 
 
 ### The main code that gets run
@@ -14,7 +25,8 @@ for i in range(13):
     star()
     penup()
     right(360/13)
-    forward(50)
+    forward(5)
     pendown()
+
 
 done()
