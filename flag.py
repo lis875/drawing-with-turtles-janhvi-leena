57dcac417da5656798f39f28b7b9c50df7cc1ad7
Leena <leena@wisc.edu>
2023-10-10 18:41:05 -0500

    Flag of Ghana

--- flag.py
+++ flag.py
@@ -2,7 +2,7 @@ from turtle import *
 
 ### Functions for drawing
 # Modify the code in this function
-
+speed(0)
 def bar(bar_color):
     begin_fill()  # Begin filling the shape
     fillcolor(bar_color)  # Set the fill color
@@ -13,9 +13,23 @@ def bar(bar_color):
         right(90)
     end_fill()  # End filling the shape
 
+def star(size):
+    begin_fill()
+    fillcolor("black")
+    for i in range(5):
+        forward(size)
+        right(144)
+    end_fill()    
+    penup()
+    forward(100)
+    pendown()
+    
+
 ### Main code that gets run, to draw the German flag
 # Do not modify code below this line for Exercise 7. It is OK to add code below this for Exercise 8
-
+penup()
+back(100)
+pendown()
 bar("black")
 right(90)
 forward(50)
@@ -26,6 +40,12 @@ forward(50)
 left(90)
 bar("yellow")
 right(90)
+
+back(30); left(90); 
+penup()
+forward(125)
+pendown()
+star(52)
 
 
 
